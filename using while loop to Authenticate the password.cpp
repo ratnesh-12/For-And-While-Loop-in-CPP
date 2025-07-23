df448d49b1fa7246020123203d549f6891383e66
Ratnesh Kumar <ratneshkr129@gmail.com>
2025-07-23 21:15:37 +0530

    Create using while loop to Authenticate the password.cpp

---
+++ using while loop to Authenticate the password.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string correctPassword = "@619";
+    string enteredPassword;
+    int number = 0;
+    int special = 0;
+
+    cout << "Enter password: ";
+    cin >> enteredPassword;
+
+    int i = 0;
+    while (i < enteredPassword.length()) {
+        char ch = enteredPassword[i];
+
+        if (ch >= '0' && ch <= '9') number++;
+        if (ch == '@' || ch == '#' || ch == '$' || ch == '!' || ch == '%') special++;
+
+        i++;
+    }
+
+    if (number == 0) cout << "Enter at least a number\n";
+    if (special == 0) cout << "Enter at least a special char\n";
+
+    if (number > 0 && special > 0) {
+        if (enteredPassword == correctPassword) {
+            cout << "Valid password\n";
+        } else {
+            cout << "Invalid password\n";
+        }
+    }
+
+    return 0;
+}
