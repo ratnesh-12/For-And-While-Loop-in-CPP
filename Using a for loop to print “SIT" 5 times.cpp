3a2426b09972cae7b00fde866c784b2bcae94cef
Ratnesh Kumar <ratneshkr129@gmail.com>
2025-07-23 21:00:22 +0530

    Create Using a for loop to print “SIT" 5 times.cpp

---
+++ Using a for loop to print “SIT" 5 times.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i=1; i<6; i++){
+        cout<<"SIT"<<endl;
+    }
+
+    return 0;
+}
+
+OUTPUT:
+
+SIT
+SIT
+SIT
+SIT
+SIT
+=== Code Execution Successful ===
